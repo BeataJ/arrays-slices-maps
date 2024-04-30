7a531176501bd1880e7827f2613fe363a5e71c2d
Beata <beata@jasinski.com>
2024-04-30 14:54:16 -0700

    make for arrays, maps

--- main.go
+++ main.go
@@ -6,9 +6,18 @@ func main() {
 	userNames := make([]string, 2, 5)
 
 	userNames[0] = "bob"
+	userNames[1] = "eve"
 
 	userNames = append(userNames, "max")
 	userNames = append(userNames, "beata")
 
 	fmt.Println(userNames)
+
+	courseRating := make(map[string]float64, 3)
+
+	courseRating["go"] = 4.7
+	courseRating["react"] = 4.8
+	courseRating["angular"] = 4.7
+
+	fmt.Println(courseRating)
 }
