9e4d043b1b6c7d88811445db1bff9b3afc403d73
Beata <beata@jasinski.com>
2024-04-30 17:25:12 -0700

    make for map

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func main() {
 	userNames := make([]string, 2, 5)
 
@@ -13,11 +19,13 @@ func main() {
 
 	fmt.Println(userNames)
 
-	courseRating := make(map[string]float64, 3)
+	courseRating := make(floatMap, 3)
 
 	courseRating["go"] = 4.7
 	courseRating["react"] = 4.8
 	courseRating["angular"] = 4.7
 
-	fmt.Println(courseRating)
+	courseRating.output()
+
+	// fmt.Println(courseRating)
 }
